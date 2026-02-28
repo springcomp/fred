d1a7e11c51b41a90b53dd5734dff932fae81518f
Springcomp <springcomp@users.noreply.github.com>
2026-02-28 09:03:48 +0100

    supports initial sample xsd

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import { Toolbar } from "./components/Toolbar";
 import { SplitPane } from "./components/SplitPane";
 import { SchemaTree } from "./components/tree/SchemaTree";
 import { PropertySheet } from "./components/properties/PropertySheet";
+import { useEditorStore } from "./store/editorStore";
+import { parseXsd } from "./model/parser";
+import { untitledXsd } from "./model/samples";
 
 export function App() {
+  const schema = useEditorStore((s) => s.schema);
+
+  useEffect(() => {
+    if (!schema) {
+      useEditorStore.getState().loadSchema(parseXsd(untitledXsd));
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div className="flex flex-col h-full">
       <Toolbar />
